fb5a431caf480e9055d08edbb631c9d107338a22
gngrninja <thegngrninja@gmail.com>
2019-08-11 10:26:53 -0700

    adding csharp help goodness

---
+++ csharp/sealionhelp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use the calculator class' static method Start (code in Calculator.cs)
+            Calculator.Start();
+        }
+    }
+}
